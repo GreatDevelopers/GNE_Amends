be721516b6f0c7e8dd3945cf5d7afbfa266dadfb
HS Rai <hardeep.rai@gmail.com>
2024-12-09 14:16:53 +0530

    Duplicate of To Do

---
+++ gne_amends/custom_gne/doctype/to_do_gne/to_do_gne.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, H S Rai and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("To Do GNE", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ gne_amends/custom_gne/doctype/to_do_gne/to_do_gne.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, H S Rai and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ToDoGNE(Document):
+	pass
