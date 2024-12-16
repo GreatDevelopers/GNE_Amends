703a38e96e59ad4da797400d97c3790451baa121
HS Rai <hardeep.rai@gmail.com>
2024-12-16 12:17:19 +0530

    Photo added to list

--- gne_amends/gne_amends/doctype/ie_members/ie_members.py
+++ gne_amends/gne_amends/doctype/ie_members/ie_members.py
@@ -1,9 +1,19 @@
 # Copyright (c) 2024, H S Rai and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 
 class IEMembers(WebsiteGenerator):
-	pass
+        pass
+
+
+#from frappe.website.website_generator import WebsiteGenerator
+
+#class IEMembers(WebsiteGenerator):
+#    def get_context(self, context):
+#        # Set the title for the page
+#        context.title = "IE Members List by HSRai"
+#        # You can add other context data here if needed
+#        return context
